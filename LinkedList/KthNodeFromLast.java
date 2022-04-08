026165277303b9794f8799c62c196e4b1b72bce2
Kiran Kumar K <33867697+kichu173@users.noreply.github.com>
2022-04-09 00:11:35 +0530

    LinkedList Class2 - 3rd-Apr

--- LinkedList/KthNodeFromLast.java
+++ LinkedList/KthNodeFromLast.java
@@ -21,3 +21,8 @@ public class KthNodeFromLast {
 
 // 2. https://leetcode.com/problems/middle-of-the-linked-list/ - completed. Brut force TC: O(n) SC: 1 | Tortoise and hare TC:O(n) SC:1
 // algo:: https://www.youtube.com/watch?v=_cl3O4FBZh8 for Q2(middle-of-the-linked-list)
+// 3. https://leetcode.com/problems/odd-even-linked-list/ - completed. TC: O(N) SC: 1 algo:: Approach 2 (https://www.youtube.com/watch?v=YE9ggKeHeK0)
+// https://leetcode.com/problems/odd-even-linked-list/discuss/1912718/O(n)O(1)-space-easy-variable-naming - try to understand this approach also | class 1:03:00
+// 4. https://leetcode.com/problems/add-two-numbers/ - completed. TC: O(max(m,n)) SC:n
+// algo:: https://www.youtube.com/watch?v=LBVsXSMOIk4
+// 5. https://leetcode.com/problems/delete-node-in-a-linked-list/ - TODO.
