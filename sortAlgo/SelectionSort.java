fdc13da5ee16d6e8be69dac03e88c0247ad0a82d
Kiran Kumar K <33867697+kichu173@users.noreply.github.com>
2022-07-30 23:26:22 +0530

    Selection Sort based on condition to sort either asc or desc

--- sortAlgo/SelectionSort.java
+++ sortAlgo/SelectionSort.java
@@ -4,25 +4,42 @@ import java.util.Arrays;
 
 public class SelectionSort {
 
-    public static void selectionSort(int[] arr) {// (worst or best case) TC:: O(n^2)
-        for (int i = 0; i < arr.length; i++) {// fix the position
-            int minIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {// select the min value
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
+    public static void selectionSort(int[] arr, boolean isAscending) {// (worst or best case) TC:: O(n^2)
+        if(isAscending) {
+            for (int i = 0; i < arr.length; i++) {// fix the position
+                int minIndex = i;
+                for (int j = i + 1; j < arr.length; j++) {// select the min value
+                    if (arr[j] < arr[minIndex]) {
+                        minIndex = j;
+                    }
                 }
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
             }
-            // swap
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                int maxIndex = i;
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[j] > arr[maxIndex]) {
+                        maxIndex = j;
+                    }
+                }
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[maxIndex];
+                arr[maxIndex] = temp;
+            }
         }
+
     }
 
 
     public static void main(String[] args) {
         int[] arr = {10, 5, 26, 3, 15, 22};
-        SelectionSort.selectionSort(arr);
+        boolean asc = false;
+        SelectionSort.selectionSort(arr, asc);
         System.out.println(Arrays.toString(arr));
     }
 }
